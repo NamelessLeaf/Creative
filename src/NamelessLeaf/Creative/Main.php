e7dedf06c5b531e8bc5b695c1f41302b37223258
NamelessLeaf <74725613+NamelessLeaf@users.noreply.github.com>
2021-02-09 14:32:11 +0000

    Update Main.php

--- src/NamelessLeaf/Creative/Main.php
+++ src/NamelessLeaf/Creative/Main.php
@@ -17,4 +17,26 @@ use jojoe77777\FormAPI;
 use pocketmine\command\CommandExecutor;
 use pocketmine\command\ConsoleCommandSender;
 
-class Main extends PluginBase implements Listener{}
+class Main extends PluginBase implements Listener{
+  
+  public function onEnable(){
+    $this->getLogger()info->("Enabled Plugin");
+  }
+  
+  public function onDisable(){
+    $this->getLogger()info->("Successfuly Disabled Plugin");
+  }
+  
+  public function onCommand(CommandSender $sender, Command $cmd, String $lable, Array $args) : bool {
+  
+    switch($cmd->getName){
+      case "creativeui":
+        if($sender instanceOf Player){
+          $form->$player:
+        }
+    }
+    
+    
+    
+  }
+}
