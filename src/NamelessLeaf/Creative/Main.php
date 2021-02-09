9ab867b787ee4f11d84ad0d5e35860f014c871ac
NamelessLeaf <74725613+NamelessLeaf@users.noreply.github.com>
2021-02-09 14:23:51 +0000

    Create Main.php

---
+++ src/NamelessLeaf/Creative/Main.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace: NamelessLeaf\Creative;
+
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerPreLoginEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use jojoe77777\FormAPI;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\ConsoleCommandSender;
+
+class Main extends pluginBase {}
